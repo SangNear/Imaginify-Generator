7df1b6785e6b39b8dd5717ee390e6b9287ac746a
Sang Near <nguyenlamsang2004@gmail.com>
2024-05-25 11:20:05 +0700

    done menu ui

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,87 @@
+"use client"
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { navLinks } from '@/contants'
+import { usePathname } from 'next/navigation'
+const MobileNav = () => {
+    const path = usePathname()
+    return (
+        <header className='p-3 flex justify-between items-center lg:hidden'>
+            <Link href="/">
+                <Image src="/assets/images/logo-text.svg" width={180} height={28} alt='logo' />
+            </Link>
+            <SignedIn>
+                <div className='flex flex-center gap-2 max-sm:gap-1'>
+                    <UserButton />
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Image src="/assets/icons/menu.svg" alt='menu' width={32} height={32} className='border w-8 rounded-lg cursor-pointer' />
+                        </SheetTrigger>
+                        <SheetContent>
+
+                            <Image src="/assets/images/logo-text.svg" alt='logo' width={128} height={28} />
+                            <nav className='flex flex-col  h-full items-baseline mt-3 items-start justify-between pb-5'>
+                                <SignedIn>
+                                    <ul className='w-full md:flex flex-col'>
+                                        {navLinks.slice(0, 6).map((link) => {
+                                            const isActive = link.route === path
+                                            return (
+                                                <li key={link.route} className={`hover:bg-purple-400 hover:shadow-inner bg-cover p-3 rounded-3xl flex justify-center transition-all cursor-pointer ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
+                                                    <Link href={link.route} className='flex size-full gap-4'>
+                                                        <Image src={link.icon} width={28} height={28} alt='icon-menu' />
+                                                        {link.label}
+                                                    </Link>
+                                                </li>
+                                            )
+                                        })}
+
+                                    </ul>
+                                    <ul className='w-full md:flex flex-col gap-4'>
+                                        {navLinks.slice(6).map((link) => {
+                                            const isActive = link.route === path
+                                            return (
+                                                <li key={link.route} className={`hover:bg-purple-400 hover:shadow-inner bg-cover p-3 rounded-3xl flex justify-center transition-all cursor-pointer ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
+                                                    <Link href={link.route} className='flex size-full gap-4'>
+                                                        <Image src={link.icon} width={28} height={28} alt='icon-menu' />
+                                                        {link.label}
+                                                    </Link>
+                                                </li>
+                                            )
+                                        })}
+                                        <li className='p-3 items-start justify-start'>
+                                            <UserButton showName />
+                                        </li>
+                                    </ul>
+                                </SignedIn>
+                                <SignedOut>
+                                    <Button asChild className='bg-purple-gradient bg-cover rounded-full hover:opacity-90 transition-all'>
+                                        <Link href="/sign-in">Login</Link>
+                                    </Button>
+                                </SignedOut>
+                            </nav>
+
+
+                        </SheetContent>
+                    </Sheet>
+                </div>
+
+            </SignedIn>
+        </header>
+    )
+}
+
+export default MobileNav
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,11 +1,16 @@
+"use client"
 import { navLinks } from '@/contants'
+import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
+    const path = usePathname()
     return (
-        <aside className='w-72 p-5 shadow-md lg:flex hidden h-screen'>
+        <aside className='w-72 p-5 shadow-md lg:flex hidden h-screen sticky left-0 top-0'>
             <div>
                 <Link href="/">
                     <Image
@@ -15,20 +20,33 @@ const Sidebar = () => {
                         height={28}
                     />
                 </Link>
-                <nav>
-                    <ul>
-                        {navLinks.map((link) => {
-                            return (
-                                <li key={link.route}>
-                                    {link.label}
-                                </li>
-                            )
-                        })}
-                    </ul>
+                <nav className='flex  justify-center h-full mt-3'>
+                    <SignedIn>
+                        <ul className='w-full md:flex flex-col gap-2'>
+                            {navLinks.map((link) => {
+                                const isActive = link.route === path
+                                return (
+                                    <li key={link.route} className={`hover:bg-purple-400 hover:shadow-inner bg-cover p-3 rounded-3xl flex justify-center transition-all cursor-pointer ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
+                                        <Link href={link.route} className='flex size-full gap-4'>
+                                            <Image src={link.icon} width={28} height={28} alt='icon-menu' />
+                                            {link.label}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                            <li className='flex-center p-3'>
+                                <UserButton showName/>
+                            </li>
+                        </ul>
+                    </SignedIn>
+                    <SignedOut>
+                        <Button asChild className='bg-purple-gradient bg-cover rounded-full hover:opacity-90 transition-all'>
+                            <Link href="/sign-in">Login</Link>
+                        </Button>
+                    </SignedOut>
                 </nav>
             </div>
         </aside>
     )
 }
-
 export default Sidebar
